7095f2a47afe23b95f729b44ce4c2ec1dc40f9ec
Winfred, Timothy (206596334) <TWinfred@fandango.com>
2021-02-10 14:42:33 -0800

    Setup a functional/stateless component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
 function App() {
-  // tuple
-  let aTuple: [string, number] = ['apple', 20];
-  // enum
-  enum Codes {first = 1, second = 2};
-  // any
-  let firstName: any = 'Tim';
-  firstName = 12345;
-  // void
-  const warning = (): void => {
-    console.log('warning!');
-  }
-  warning();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The value { Codes.first } is of { typeof Codes.first } type
-        </p>
+        <Message message='This is a message' />
       </header>
     </div>
   );
